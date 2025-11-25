2b09b2ba2dc6e69cf89cade370fff4c54d69ceb7
Abigail-MQ <abigail.mamani@usil.pe>
2025-11-25 00:47:01 -0500

    Update SistemaTaller.java
    Se usa para tener una sola instancia general del taller
    
    Signed-off-by: Abigail-MQ <abigail.mamani@usil.pe>

--- taller/src/main/java/patrones/SistemaTaller.java
+++ taller/src/main/java/patrones/SistemaTaller.java
@@ -9,5 +9,18 @@ package patrones;
  * @author HUAWEI
  */
 public class SistemaTaller {
-    
+    private static SistemaTaller instancia;
+
+    private SistemaTaller () {}
+
+    public static SistemaTaller getInstancia() {
+        if (instancia == null) {
+            instancia = new SistemaTaller();
+        }
+        return instancia;
+    }
+
+    public void iniciar() {
+        System.out.println("Sistema del taller iniciado correctamente. ");
+    }
 }
