db089e815473bd404f8258cb914ee8cd338648d6
Abigail-MQ <abigail.mamani@usil.pe>
2025-11-25 00:21:09 -0500

    Update NotificadorStock.java
    Notifica cuando el stock de repuestos baja de cierto límite
    
    Signed-off-by: Abigail-MQ <abigail.mamani@usil.pe>

--- taller/src/main/java/patrones/NotificadorStock.java
+++ taller/src/main/java/patrones/NotificadorStock.java
@@ -4,10 +4,29 @@
  */
 package patrones;
 
+import java.uti.ArrayList;
+
 /**
  *
  * @author HUAWEI
  */
-public class NotificadorStock {
-    
+public class NotificadorStock implements Sujeto {
+    private ArrayList<Observador> observadores = new ArrayList<>();
+
+    @Override
+    public void agregarObservador(Observador o){
+        observadores.add(o);
+    }
+
+    @Override
+    public void quitarObservador(Observador o){
+        observadores.remove(o);
+    }
+
+    @Override
+    public void notificar (String mensaje){
+        for (Observador o: observadores){
+            o.actualizar(mensaje);
+        }
+    }
 }
